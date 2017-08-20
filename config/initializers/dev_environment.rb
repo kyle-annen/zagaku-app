6705942ca25fb3f18b47a1d070cf8ea96d54bcdc
Kyle Annen <kyleannen@gmail.com>
2017-08-20 13:27:59 -0500

    change to rails

---
+++ config/initializers/dev_environment.rb
@@ -0,0 +1,6 @@
+=begin
+unless Rails.env.production?
+  ENV['GOOGLE_CLIENT_ID'] = '390664386123-vugr764385c24nl2q8c5fj3njbatvp1u.apps.googleusercontent.com'
+  ENV['GOOGLE_CLIENT_SECRET'] = '7JBis7ny_cNqkKjtkzunOigH'
+end
+=end
